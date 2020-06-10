37160f54d0fe41c54f7615142795acb4d1292670
Siddharth Samsi <samsis@mit.edu>
2020-06-10 16:35:05 -0400

    removed default_args from utils.py

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -41,35 +41,3 @@ def log_args(args, logger_name=None):
         logger.info("{0: <15} : {1:}".format(arg, getattr(args, arg)))
     return
 
-def default_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--model', type=str, default='unet',
-                            help='name of model to train',
-                            choices=['unet', 'nowcast', 'synrad', 'nowcast_gan'])
-    parser.add_argument('--loss_fn', type=str, default='mse',
-                            choices=['mse', 'vil', 'multilevel-vil', 'vgg', 'myloss', 'ce', 'ssim',
-                                         'mse+vil', 'mse+multilevel-vil', 'mse+vgg', 'multiscale',
-                                         'mse+ssim', 'featureloss', 'gan', 'binary_crossentropy'])
-    parser.add_argument('--loss_weights', nargs='+', default="[1.0]")
-    parser.add_argument('--train_data', type=str, help='path to training data file')
-    parser.add_argument('--test_data', type=str, help='path to test data file')
-    parser.add_argument('--nepochs', type=int, help='number of epochs', default=50)    
-    parser.add_argument('--batch_size', type=int, help='batch size', default=8)
-    parser.add_argument('--num_train', type=int, help='number of training sequences to read',
-                            default=2048)
-    parser.add_argument('--test_start', type=int, help='where to start reading test data from',
-                            default=4096)
-    parser.add_argument('--num_test', type=float, help='number of test sequences to read',
-                            default=8)
-    parser.add_argument('--lr', type=float, help='learning rate', default=0.000001)
-    parser.add_argument('--verbosity', type=int, default=2)
-    parser.add_argument('--no_hvd', action='store_true', help='disable Horovod')
-    parser.add_argument('--logdir', type=str, help='log directory', default='./logs')
-    parser.add_argument('--num_warmup', type=int, help='number of warmup epochs', default=2)
-    parser.add_argument('--gen_weights', type=str, help='path to pre-trained unet/generator weights',
-                            default=None)
-    parser.add_argument('--disc_weights', type=str, help='path to pre-trained discriminator weights',
-                            default=None)
-    args, unknown = parser.parse_known_args()
-
-    return args
