9afb00dd6984a956329d989c144a899f4b31d866
Siddharth Samsi <samsis@mit.edu>
2020-06-10 22:45:42 -0400

    Changed make_callback_dirs function signature

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -41,13 +41,16 @@ def log_args(args, logger_name=None):
         logger.info("{0: <15} : {1:}".format(arg, getattr(args, arg)))
     return
 
-def make_callback_dirs(tensorboard_dir, imgs_dir, weights_dir):
+def make_callback_dirs(logdir):
+    tensorboard_dir = os.path.join(logdir, 'tensorboard')
     if not os.path.isdir(tensorboard_dir):
         os.makedirs(tensorboard_dir)
 
+    imgs_dir = os.path.join(logdir, 'images')
     if not os.path.isdir(imgs_dir):
         os.makedirs(imgs_dir)
-
+    
+    weights_dir = os.path.join(logdir, 'weights')
     if not os.path.isdir(weights_dir):
         os.makedirs(weights_dir)
     return tensorboard_dir, imgs_dir, weights_dir
