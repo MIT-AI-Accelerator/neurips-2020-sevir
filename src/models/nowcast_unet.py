01b6b2f2ee356d4bccae2d33c82695f24c63c1bf
Siddharth Samsi <samsis@mit.edu>
2020-06-11 08:44:46 -0400

    removed unused function

--- src/models/nowcast_unet.py
+++ src/models/nowcast_unet.py
@@ -21,31 +21,3 @@ def create_model(input_shape=(384,384,13), num_outputs=12, activation='relu'):
     
     return inputs, outputs
 
-
-def create_model_v2(input_shape=(384,384,13), num_outputs=12,
-                        num_encoders=4, filter_sz=[32, 64, 128, 256],
-                        center_sz=1024, activation='relu'):
-
-    inputs = layers.Input(shape=input_shape)
-    encoder = num_encoders*list([None])
-    decoder = num_encoders*list([None])
-    encoder_pool = num_encoders*list([None])
-    encoder_pool[0], encoder[0] = encoder_block(inputs, filter_sz[0], activation=activation)
-    for ii in range(1,num_encoders):
-        encoder_pool[ii], encoder[ii] = encoder_block(encoder_pool[ii-1],
-                                                          filter_sz[ii],
-                                                          activation=activation)
-    center = conv_block(encoder_pool[-1], center_sz)
-    decoder[0] = decoder_block(center, encoder[-1], filter_sz[-1])
-    for ii in range(num_encoders-2,-1,-1):
-        decoder[num_encoders-ii-1] = decoder_block(decoder[num_encoders-ii-2],
-                                                     encoder[ii],
-                                                     filter_sz[ii],
-                                                     activation=activation)
-        
-    outputs = layers.Conv2D(num_outputs, (1, 1),
-                                padding='same',
-                                activation='linear',
-                                name='output_layer')(decoder[-1])
-
-    return inputs,outputs
